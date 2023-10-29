3f9750d7e62ef768ade2b66f859d430e3988a812
atielijohn8 <atielijohn4@gmail.com>
2023-10-29 09:58:54 +0300

    list

--- data_structure/index.py
+++ data_structure/index.py
@@ -4,6 +4,6 @@ print(basket.index('d'))
 #print(basket.index('d',0,3))
 print(basket.index('d',0,4))#start from 0-4
 
-print('b' in basket)#search something(also works in string)
+print('b' in basket)#search exisitance of something(also works in string)
 print('x' in basket)
 print(basket.count('d'))#counts occurrence
