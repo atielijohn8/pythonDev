865960bd24fbb43c9508972a5432334c596ff3b9
atielijohn8 <atielijohn4@gmail.com>
2023-10-29 06:22:16 +0300

    list

---
+++ data_structure/index.py
@@ -0,0 +1,9 @@
+basket = ['a','b','c','d','e']
+
+print(basket.index('d'))
+#print(basket.index('d',0,3))
+print(basket.index('d',0,4))#start from 0-4
+
+print('b' in basket)#search something(also works in string)
+print('x' in basket)
+print(basket.count('d'))#counts occurrence
---
+++ data_structure/list_methods.py
@@ -0,0 +1,11 @@
+basket = [1,2,3,4,5,]
+
+#basket.append(100)#adds 100
+#basket.remove(4)#removes 4
+#basket.pop(0)#removes 1
+#basket.clear()
+
+new_list = basket.pop(0)
+
+print(basket)
+print(new_list)#returns what it removed
